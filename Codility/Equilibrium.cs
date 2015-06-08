ee66551bc2bfbc9e013de4353627721d9ba08fbc
Leo Li <gqy117@gmail.com>
2015-06-08 20:52:27 +0800

    [] First version.

---
+++ Codility/Equilibrium.cs
@@ -0,0 +1,32 @@
+﻿namespace Codility
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Equilibrium
+    {
+        public static int Solution(int[] a)
+        {
+            long sumAll = a.Select(x => (long)x).Sum();
+            long sumLeft = 0;
+
+            for (int balanceNumber = 0; balanceNumber < a.Length; balanceNumber++)
+            {
+                int balanceValue = a[balanceNumber];
+
+                long sumRight = sumAll - sumLeft - balanceValue;
+
+                if (sumLeft == sumRight)
+                {
+                    return balanceNumber;
+                }
+
+                sumLeft += balanceValue;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ CodilityTest/EquilibriumTest.cs
@@ -0,0 +1,54 @@
+﻿namespace CodilityTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Codility;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EquilibriumTest
+    {
+        [Test]
+        [TestCase(new int[] { -1, 3, -4, 5, 1, -6, 2, 1 }, 1)]
+        public void Equilibrium_DemoTest(int[] a, int expected)
+        {
+            ExecuteSolution(a, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { -2147483648 }, 0)]
+        public void Equilibrium_BigNegativeNumber(int[] a, int expected)
+        {
+            ExecuteSolution(a, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 500, 1, -2, -1, 2 }, 0)]
+        public void Equilibrium_LeftFirst(int[] a, int expected)
+        {
+            ExecuteSolution(a, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 2147483647, 2147483647, 0, 2147483647, 2147483647 }, 2)]
+        public void Equilibrium_BigPositiveNumber(int[] a, int expected)
+        {
+            ExecuteSolution(a, expected);
+        }
+
+        private static void ExecuteSolution(int[] a, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Equilibrium.Solution(a);
+
+            // Assert
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
