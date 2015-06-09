b2485c82ba74e41ad68cb00c7947a70eb2666871
Leo Li <gqy117@gmail.com>
2015-06-09 20:50:07 +0800

    [MaxCounters] Implemented MaxCounters.cs.

--- Codility/Lesson2/MaxCounters.cs
+++ Codility/Lesson2/MaxCounters.cs
@@ -8,9 +8,37 @@
 
     public class MaxCounters
     {
-        public static int Solution(int X, int Y, int D)
+        public static int[] Solution(int N, int[] A)
         {
-            throw new NotImplementedException();
+            int[] counters = new int[N];
+            for (int i = 0; i < counters.Length; i++)
+            {
+                counters[i] = 0;
+            }
+
+            int currentMax = 0;
+            int currentCountValue = 0;
+
+            for (int K = 0; K < A.Length; K++)
+            {
+                int X = A[K];
+
+                if (1 <= X && X <= N)
+                {
+                    counters[X - 1]++;
+                    currentCountValue = counters[X - 1];
+                    currentMax = Math.Max(currentMax, currentCountValue);
+                }
+                else
+                {
+                    for (int i = 0; i < counters.Length; i++)
+                    {
+                        counters[i] = currentMax;
+                    }
+                }
+            }
+
+            return counters;
         }
     }
 }
