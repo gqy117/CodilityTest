85ce9d1a53a18c30b139a9cf49e83947e22aee10
Leo Li <gqy117@gmail.com>
2015-06-09 23:11:59 +0800

    [FrogRiverOne] Implemented FrogRiverOne.

---
+++ Codility/Lesson2/FrogRiverOne.cs
@@ -0,0 +1,36 @@
+﻿namespace Codility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FrogRiverOne
+    {
+        public static int Solution(int X, int[] A)
+        {
+            IList<int> expectedList = new List<int>();
+
+            for (int i = 0; i < X; i++)
+            {
+                expectedList.Add(i + 1);
+            }
+
+            for (int K = 0; K < A.Length; K++)
+            {
+                if (expectedList.Contains(A[K]))
+                {
+                    expectedList.Remove(A[K]);
+                }
+
+                if (0 == expectedList.Count)
+                {
+                    return K;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
