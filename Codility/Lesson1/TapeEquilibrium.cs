ed71f64fea3adc95b30b7ab2814029f728b352e7
Leo Li <gqy117@gmail.com>
2015-06-09 13:16:52 +0800

    TapeEquilibrium.

---
+++ Codility/Lesson1/TapeEquilibrium.cs
@@ -0,0 +1,28 @@
+﻿namespace Codility.Lesson1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TapeEquilibrium
+    {
+        public static int Solution(int[] A)
+        {
+            int n = A.Length;
+            long sumLeft = 0;
+            long sumRight = A.Sum() - sumLeft;
+            IList<long> listDifference = new List<long>();
+
+            for (int p = 1; p < n; p++)
+            {
+                sumLeft += A[p - 1];
+                sumRight -= A[p - 1];
+                listDifference.Add(Math.Abs(sumLeft - sumRight));
+            }
+
+            return (int)listDifference.Min();
+        }
+    }
+}
