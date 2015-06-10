cee8cfaa7324469267df4d9cddc1780efa2cfbff
Leo Li <gqy117@gmail.com>
2015-06-10 20:56:05 +0800

    [MinAvgTwoSlice] Added basic files.

---
+++ Codility/Lesson3/MinAvgTwoSlice/MinAvgTwoSlice.cs
@@ -0,0 +1,16 @@
+﻿namespace Codility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MinAvgTwoSlice
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson3/MinAvgTwoSliceTest.cs
@@ -0,0 +1,32 @@
+﻿namespace CodilityTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MinAvgTwoSliceTest
+    {
+        [Test]
+        [TestCase(4, 2, 2, 5, 1, 5, 8, 1)]
+        public void MaxCounters_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MinAvgTwoSlice.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
