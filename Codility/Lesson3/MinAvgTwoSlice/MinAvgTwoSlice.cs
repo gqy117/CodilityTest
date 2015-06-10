dc582fd73b976f965504812d68b6b4d16e0b7397
Leo Li <gqy117@gmail.com>
2015-06-10 22:26:37 +0800

    [MinAvgTwoSlice] Added the perfect solution.

--- Codility/Lesson3/MinAvgTwoSlice/MinAvgTwoSlice.cs
+++ Codility/Lesson3/MinAvgTwoSlice/MinAvgTwoSlice.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
@@ -10,7 +9,34 @@
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            int minP = int.MaxValue;
+            double minAvg = int.MaxValue;
+            double currentAvg;
+
+            for (int P = 0; P < N - 1; P++)
+            {
+                int Q = P + 1;
+
+                double currentAvg2 = (double)(A[P] + A[Q]) / 2;
+                if (Q + 1 < N)
+                {
+                    double currentAvg3 = (double)(A[P] + A[Q] + A[Q + 1]) / 3;
+                    currentAvg = Math.Min(currentAvg2, currentAvg3);
+                }
+                else
+                {
+                    currentAvg = currentAvg2;
+                }
+
+                if (currentAvg < minAvg)
+                {
+                    minAvg = currentAvg;
+                    minP = P;
+                }
+            }
+
+            return minP;
         }
     }
 }
--- CodilityTest/Lesson3/MinAvgTwoSliceTest.cs
+++ CodilityTest/Lesson3/MinAvgTwoSliceTest.cs
@@ -12,9 +12,36 @@
     public class MinAvgTwoSliceTest
     {
         [Test]
-        [TestCase(4, 2, 2, 5, 1, 5, 8, 1)]
+        [TestCase(new int[] { 4, 2, 2, 5, 1, 5, 8 }, 1)]
+        [TestCase(new int[] { 1, 2, 3 }, 0)]
+        [TestCase(new int[] { 3, 2, 1 }, 1)]
+        [TestCase(new int[] { 1, 2, 9, 1, 2, 1 }, 3)]
         public void MaxCounters_Default(int[] A, int expected)
         {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 10000, 10000 }, 0)]
+        [TestCase(new int[] { -10000, -10000 }, 0)]
+        [TestCase(new int[] { -10000, 10000 }, 0)]
+        [TestCase(new int[] { 10000, -10000 }, 0)]
+        public void MaxCounters_N2_Max_Min(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(100000, 10000, 0)]
+        [TestCase(100000, -10000, 0)]
+        public void MaxCounters_N100000_Max_Min(int N, int element, int expected)
+        {
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = element;
+            }
+
             ExecuteSolution(A, expected);
         }
 
