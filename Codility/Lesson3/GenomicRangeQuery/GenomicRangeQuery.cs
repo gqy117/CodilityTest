b8b4ea4fbfcfaa54f78d9e1b25944937f1cd9ce3
Leo Li <gqy117@gmail.com>
2015-06-11 20:42:01 +0800

    [GenomicRangeQuery] GenomicRangeQuery basic version.

---
+++ Codility/Lesson3/GenomicRangeQuery/GenomicRangeQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace Codility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GenomicRangeQuery
+    {
+        public static int Solution(string S, int[] P, int[] Q)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson3/GenomicRangeQueryTest.cs
@@ -0,0 +1,32 @@
+﻿namespace CodilityTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GenomicRangeQueryTest
+    {
+        [Test]
+        [TestCase("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 }, 1)]
+        public void MaxCounters_Default(string S, int[] P, int[] Q, int expected)
+        {
+            ExecuteSolution(S, P, Q, expected);
+        }
+
+        private static void ExecuteSolution(string S, int[] P, int[] Q, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = GenomicRangeQuery.Solution(S, P, Q);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
