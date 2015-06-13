c7f9fd754cebbf3dfa2d5727050f0371edc8be15
Leo Li <gqy117@gmail.com>
2015-06-13 20:39:01 +0800

    [PassingCars] 70% solution.

--- Codility/Lesson3/PassingCars/PassingCars.cs
+++ Codility/Lesson3/PassingCars/PassingCars.cs
@@ -6,7 +6,53 @@
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            int pairCount = 0;
+            int countP = 0;
+            bool zeroFirst = 0 == A[0];
+
+            for (int i = 0; i < N; i++)
+            {
+                if (pairCount > 1000000000)
+                {
+                    return -1;
+                }
+
+                if (zeroFirst)
+                {
+                    switch (A[i])
+                    {
+                        case 0:
+                            countP++;
+                            break;
+
+                        case 1:
+                            pairCount += countP;
+                            break;
+
+                        default:
+                            throw new IndexOutOfRangeException();
+                    }
+                }
+                else
+                {
+                    switch (A[i])
+                    {
+                        case 1:
+                            countP++;
+                            break;
+
+                        case 0:
+                            pairCount += countP;
+                            break;
+
+                        default:
+                            throw new IndexOutOfRangeException();
+                    }
+                }
+            }
+
+            return pairCount;
         }
     }
 }
--- CodilityTest/Lesson3/PassingCarsTest.cs
+++ CodilityTest/Lesson3/PassingCarsTest.cs
@@ -7,12 +7,65 @@
     public class PassingCarsTest
     {
         [Test]
-        [TestCase(new int[] { 4, 2, 2, 5, 1, 5, 8 }, 1)]
-        [TestCase(new int[] { 1, 2, 3 }, 0)]
-        [TestCase(new int[] { 3, 2, 1 }, 1)]
-        [TestCase(new int[] { 1, 2, 9, 1, 2, 1 }, 3)]
+        [TestCase(new int[] { 0, 1, 0, 1, 1 }, 5)]
         public void MaxCounters_Default(int[] A, int expected)
         {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 0, 1, 0, 0 }, 5)]
+        public void MaxCounters_1First(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 0, 1 }, 1)]
+        public void MaxCounters_3Pairs(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 0, 1, 0 }, 3)]
+        public void MaxCounters_4Pairs(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 1 }, 0)]
+        [TestCase(new int[] { 0, 0 }, 0)]
+        [TestCase(new int[] { 0, 1 }, 1)]
+        [TestCase(new int[] { 1, 0 }, 1)]
+        public void MaxCounters_Double(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1 }, 0)]
+        [TestCase(new int[] { 0 }, 0)]
+        public void MaxCounters_Single(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(100000, -1)]
+        public void MaxCounters_N100000(int N, int expected)
+        {
+            int[] A = new int[100000];
+            int zeroOrOne = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = zeroOrOne;
+
+                zeroOrOne = zeroOrOne == 0 ? 1 : 0;
+            }
+
             ExecuteSolution(A, expected);
         }
 
