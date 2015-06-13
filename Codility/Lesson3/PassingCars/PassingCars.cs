bf54819ea91825c8352e14e388eacc7c55f41900
Leo Li <gqy117@gmail.com>
2015-06-13 18:24:48 +0800

    [PassingCars] Added files.

---
+++ Codility/Lesson3/PassingCars/PassingCars.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class PassingCars
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson3/PassingCarsTest.cs
@@ -0,0 +1,30 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PassingCarsTest
+    {
+        [Test]
+        [TestCase(new int[] { 4, 2, 2, 5, 1, 5, 8 }, 1)]
+        [TestCase(new int[] { 1, 2, 3 }, 0)]
+        [TestCase(new int[] { 3, 2, 1 }, 1)]
+        [TestCase(new int[] { 1, 2, 9, 1, 2, 1 }, 3)]
+        public void MaxCounters_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = PassingCars.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
