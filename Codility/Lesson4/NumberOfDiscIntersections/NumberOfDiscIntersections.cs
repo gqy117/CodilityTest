29261b0cffd1e48880824f5e8007b0c234b09a47
Leo Li <gqy117@gmail.com>
2015-06-13 20:47:31 +0800

    [NumberOfDiscIntersections] Added files.

---
+++ Codility/Lesson4/NumberOfDiscIntersections/NumberOfDiscIntersections.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class NumberOfDiscIntersections
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson4/NumberOfDiscIntersectionsTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NumberOfDiscIntersectionsTest
+    {
+        [Test]
+        [TestCase(new int[] { 1, 5, 2, 1, 4, 0 }, 11)]
+        public void MaxCounters_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = NumberOfDiscIntersections.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
