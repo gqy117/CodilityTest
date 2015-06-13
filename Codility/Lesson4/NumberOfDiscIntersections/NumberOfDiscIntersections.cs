40affc41ac6befddc8c666560fdd899bc8dfd739
Leo Li <gqy117@gmail.com>
2015-06-13 21:51:41 +0800

    [NumberOfDiscIntersections] Correctness 100% Performance 62%

--- Codility/Lesson4/NumberOfDiscIntersections/NumberOfDiscIntersections.cs
+++ Codility/Lesson4/NumberOfDiscIntersections/NumberOfDiscIntersections.cs
@@ -6,7 +6,27 @@
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            int intersectedCount = 0;
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    long radiusOfLeftRound = A[i];
+                    long maxOfLeftRound = i + radiusOfLeftRound;
+
+                    long radiusOfRightRound = A[j];
+                    long minOfRightRound = j - radiusOfRightRound;
+
+                    if (maxOfLeftRound >= minOfRightRound)
+                    {
+                        intersectedCount++;
+                    }
+                }
+            }
+
+            return intersectedCount;
         }
     }
 }
--- CodilityTest/Lesson4/NumberOfDiscIntersectionsTest.cs
+++ CodilityTest/Lesson4/NumberOfDiscIntersectionsTest.cs
@@ -8,7 +8,30 @@
     {
         [Test]
         [TestCase(new int[] { 1, 5, 2, 1, 4, 0 }, 11)]
-        public void MaxCounters_Default(int[] A, int expected)
+        public void NumberOfDiscIntersections_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1 }, 0)]
+        [TestCase(new int[] { 4 }, 0)]
+        public void NumberOfDiscIntersections_N1(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3 }, 3)]
+        [TestCase(new int[] { 0, 0, 0 }, 0)]
+        public void NumberOfDiscIntersections_RandomSmall(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 0, 2147483647, 0 }, 2)]
+        public void NumberOfDiscIntersections_BigNumber(int[] A, int expected)
         {
             ExecuteSolution(A, expected);
         }
