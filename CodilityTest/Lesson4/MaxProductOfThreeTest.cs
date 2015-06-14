a7224c4e042a1788f27eed1ff496046f95ce14c8
Leo Li <gqy117@gmail.com>
2015-06-14 13:36:30 +0800

    [MaxProductOfThree] Added files.

---
+++ CodilityTest/Lesson4/MaxProductOfThreeTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class MaxProductOfThreeTest
+    {
+        [Test]
+        [TestCase(new int[] { -3, 1, 2, -2, 5, 6 }, 60)]
+        public void MaxProductOfThree_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MaxProductOfThree.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
