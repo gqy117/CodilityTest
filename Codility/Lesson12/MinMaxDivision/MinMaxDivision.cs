d4bdd9a8997ad44e3376468d90a4f7af0f149f40
Leo Li <gqy117@gmail.com>
2015-06-14 14:32:46 +0800

    [MinMaxDivision] Added files.

---
+++ Codility/Lesson12/MinMaxDivision/MinMaxDivision.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class MinMaxDivision
+    {
+        public static int Solution(int K, int M, int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson12/MinMaxDivisionTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MinMaxDivisionTest
+    {
+        [Test]
+        [TestCase(3, 5, new int[] { 2, 1, 5, 1, 2, 2, 2 }, 6)]
+        public void MinMaxDivision_Default(int K, int M, int[] A, int expected)
+        {
+            ExecuteSolution(K, M, A, expected);
+        }
+
+        private static void ExecuteSolution(int K, int M, int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MinMaxDivision.Solution(K, M, A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
