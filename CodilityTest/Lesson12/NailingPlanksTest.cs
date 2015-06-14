61ab7842f039676505ae7e016161abd2ffbd44e6
Leo Li <gqy117@gmail.com>
2015-06-14 20:15:57 +0800

    [NailingPlanks] Correctness 75% Performance 25%

--- CodilityTest/Lesson12/NailingPlanksTest.cs
+++ CodilityTest/Lesson12/NailingPlanksTest.cs
@@ -8,11 +8,84 @@
     {
         [Test]
         [TestCase(
-            new int[] { 1, 4, 5, 8 }, 
-            new int[] { 4, 5, 9, 10 }, 
-            new int[] { 4, 6, 7, 10, 2 }, 
-            4)]
+            new int[] { 1, 4, 5, 8 },
+            new int[] { 4, 5, 9, 10 },
+            new int[] { 4, 6, 7, 10, 2 },
+            4)
+        ]
         public void NailingPlanks_Default(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        [Test]
+        [TestCase(
+            new int[] { 1, 4, 5 },
+            new int[] { 4, 5, 9 },
+            new int[] { 4, 6, 7, 10, 2 },
+            2)
+        ]
+        public void NailingPlanks_2(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        [Test]
+        [TestCase(
+            new int[] { 1, 5, 7 },
+            new int[] { 4, 6, 9 },
+            new int[] { 4, 7, 8, 10, 2 },
+            -1)
+        ]
+        public void NailingPlanks_NotPossible2(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        [Test]
+        [TestCase(
+            new int[] { 1, 5, 8 },
+            new int[] { 4, 6, 9 },
+            new int[] { 4, 7 },
+            -1)
+        ]
+        public void NailingPlanks_NotEnoughNails(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        [Test]
+        [TestCase(
+            new int[] { 1, 5, 20 },
+            new int[] { 4, 6, 21 },
+            new int[] { 4, 7, 8, 9, 10, 11, 12, 20 },
+            -1)
+        ]
+        public void NailingPlanks_WastedALotOfNails(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        [Test]
+        [TestCase(
+            new int[] { 1 },
+            new int[] { 4 },
+            new int[] { 5 },
+            -1)
+        ]
+        public void NailingPlanks_NotPossible(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        [Test]
+        [TestCase(
+            new int[] { 1 },
+            new int[] { 4 },
+            new int[] { 3, 5 },
+            1)
+        ]
+        public void NailingPlanks_OneNumberPossible(int[] A, int[] B, int[] C, int expected)
         {
             ExecuteSolution(A, B, C, expected);
         }
