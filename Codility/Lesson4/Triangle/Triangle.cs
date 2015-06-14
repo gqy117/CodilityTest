251c99c0e541f8e3db4ea12d8dfa2dc8fab32c56
Leo Li <gqy117@gmail.com>
2015-06-14 12:12:27 +0800

    [Triangle] Added files.

---
+++ Codility/Lesson4/Triangle/Triangle.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class Triangle
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson4/TriangleTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class TriangleTest
+    {
+        [Test]
+        [TestCase(new int[] { 1, 5, 2, 1, 4, 0 }, 11)]
+        public void Triangle_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Triangle.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
