17313623c4e92d40ec049a5636d1d7d3ab16ba36
Leo Li <gqy117@gmail.com>
2015-06-14 18:46:42 +0800

    [NailingPlanks] Added files.

---
+++ Codility/Lesson12/NailingPlanks/NailingPlanks.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class NailingPlanks
+    {
+        public static int Solution(int[] A, int[] B, int[] C)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson12/NailingPlanksTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NailingPlanksTest
+    {
+        [Test]
+        [TestCase(
+            new int[] { 1, 4, 5, 8 }, 
+            new int[] { 4, 5, 9, 10 }, 
+            new int[] { 4, 6, 7, 10, 2 }, 
+            4)]
+        public void NailingPlanks_Default(int[] A, int[] B, int[] C, int expected)
+        {
+            ExecuteSolution(A, B, C, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int[] B, int[] C, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = NailingPlanks.Solution(A, B, C);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
