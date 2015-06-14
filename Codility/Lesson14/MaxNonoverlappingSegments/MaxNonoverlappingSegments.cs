5ae6e3111c1bdff6ac1a7f71ce887ff6fef8973a
Leo Li <gqy117@gmail.com>
2015-06-14 09:30:59 +0800

    [MaxNonoverlappingSegments] Added files.

---
+++ Codility/Lesson14/MaxNonoverlappingSegments/MaxNonoverlappingSegments.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class MaxNonoverlappingSegments
+    {
+        public static int Solution(int[] A, int[] B)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson14/MaxNonoverlappingSegmentsTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class MaxNonoverlappingSegmentsTest
+    {
+        [Test]
+        [TestCase(new int[] { 1, 3, 7, 9, 9 }, new int[] { 5, 6, 8, 9, 10 }, 3)]
+        public void MaxNonoverlappingSegments_Default(int[] A, int[] B, int expected)
+        {
+            ExecuteSolution(A, B, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int[] B, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MaxNonoverlappingSegments.Solution(A, B);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
