6987a80f7bf692c49c71c2bdcf39f73be01078e3
Leo Li <gqy117@gmail.com>
2015-06-14 13:24:57 +0800

    [Triangle] 100% solution.

--- CodilityTest/Lesson4/TriangleTest.cs
+++ CodilityTest/Lesson4/TriangleTest.cs
@@ -13,6 +13,31 @@
             ExecuteSolution(A, expected);
         }
 
+        [Test]
+        [TestCase(new int[] { }, 0)]
+        [TestCase(new int[] { 1 }, 0)]
+        [TestCase(new int[] { 1, 2 }, 0)]
+        [TestCase(new int[] { 1, 2, 3 }, 0)]
+        [TestCase(new int[] { 1, 2, 3, 4 }, 1)]
+        public void Triangle_N01234(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 2147483645, 2147483646, 2147483647 }, 1)]
+        public void Triangle_AMax(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { -2147483645, -2147483646, -2147483647 }, 0)]
+        public void Triangle_AMin(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
         private static void ExecuteSolution(int[] A, int expected)
         {
             // Arrange
