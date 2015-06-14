d69082a3979b3c2fcb01db4d3ddb07f082744e4f
Leo Li <gqy117@gmail.com>
2015-06-14 21:29:24 +0800

    [MaxSliceSum] Added files for MaxSliceSum.

---
+++ Codility/Lesson7/MaxSliceSum/MaxSliceSum.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class MaxSliceSum
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson7/MaxSliceSumTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MaxSliceSumTest
+    {
+        [Test]
+        [TestCase(new int[] { 3, 2, -6, 4, 0 }, 5)]
+        public void MaxSliceSum_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MaxSliceSum.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
