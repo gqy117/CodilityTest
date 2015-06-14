282d58c0f09b805f60e83478d680d35da2ad6dc7
Leo Li <gqy117@gmail.com>
2015-06-14 12:03:15 +0800

    [Distinct] The perfect linq solution:)

--- Codility/Lesson4/Distinct/Distinct.cs
+++ Codility/Lesson4/Distinct/Distinct.cs
@@ -1,12 +1,18 @@
 ﻿namespace Codility
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
 
     public class Distinct
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            IList<int> distinctedA = A.Distinct().ToList();
+
+            return distinctedA.Count;
         }
     }
 }
--- CodilityTest/Lesson4/DistinctTest.cs
+++ CodilityTest/Lesson4/DistinctTest.cs
@@ -12,6 +12,55 @@
             ExecuteSolution(A, expected);
         }
 
+        [Test]
+        [TestCase(new int[] { }, 0)]
+        public void DistinctTest_N0(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(100000, 100000)]
+        public void DistinctTest_N100000_AEqualsI(int N, int expected)
+        {
+            int[] A = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = i;
+            }
+
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(100000, 1)]
+        public void DistinctTest_N100000_AMax(int N, int expected)
+        {
+            int[] A = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = 1000000;
+            }
+
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(100000, 1)]
+        public void DistinctTest_N100000_AMin(int N, int expected)
+        {
+            int[] A = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = -1000000;
+            }
+
+            ExecuteSolution(A, expected);
+        }
+
         private static void ExecuteSolution(int[] A, int expected)
         {
             // Arrange
