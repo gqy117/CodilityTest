323da91f00b4d0bb3d234cc7e0c6f5938cafd73a
Leo Li <gqy117@gmail.com>
2015-06-14 11:41:25 +0800

    [TieRopes] The perfect solution.

--- Codility/Lesson14/TieRopes/TieRopes.cs
+++ Codility/Lesson14/TieRopes/TieRopes.cs
@@ -6,7 +6,21 @@
     {
         public static int Solution(int K, int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            int currentLenghSum = 0;
+            int result = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                currentLenghSum += A[i];
+                if (currentLenghSum >= K)
+                {
+                    result++;
+                    currentLenghSum = 0;
+                }
+            }
+
+            return result;
         }
     }
 }
--- CodilityTest/Lesson14/TieRopesTest.cs
+++ CodilityTest/Lesson14/TieRopesTest.cs
@@ -12,6 +12,23 @@
             ExecuteSolution(K, A, expected);
         }
 
+        [Test]
+        [TestCase(1, new int[] { 1 }, 1)]
+        [TestCase(2, new int[] { 1 }, 0)]
+        [TestCase(1, new int[] { 2 }, 1)]
+        public void MaxNonoverlappingSegments_N1_K1(int K, int[] A, int expected)
+        {
+            ExecuteSolution(K, A, expected);
+        }
+
+        [Test]
+        [TestCase(1000000000, new int[] { 1 }, 0)]
+        [TestCase(1000000000, new int[] { 1000000000 }, 1)]
+        public void MaxNonoverlappingSegments_N1_KMax(int K, int[] A, int expected)
+        {
+            ExecuteSolution(K, A, expected);
+        }
+
         private static void ExecuteSolution(int K, int[] A, int expected)
         {
             // Arrange
