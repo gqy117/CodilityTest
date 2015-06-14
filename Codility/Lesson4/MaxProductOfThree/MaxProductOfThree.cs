acd0c0e520fcac264b998eb77c4b2148aa02168b
Leo Li <gqy117@gmail.com>
2015-06-14 14:17:26 +0800

    [MaxProductOfThree] Perfect solution with negative cases.

--- Codility/Lesson4/MaxProductOfThree/MaxProductOfThree.cs
+++ Codility/Lesson4/MaxProductOfThree/MaxProductOfThree.cs
@@ -1,12 +1,29 @@
 ﻿namespace Codility
 {
     using System;
+    using System.Linq;
 
     public class MaxProductOfThree
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            var sortedA = A.OrderByDescending(x => x).ToList();
+
+            int positiveMax = sortedA.Take(3).Aggregate((current, next) => current * next);
+
+            int negativeMax = 0;
+
+            int last1 = sortedA[N - 1];
+            int last2 = sortedA[N - 2];
+            int first1 = sortedA[0];
+
+            if (last1 < 0 && last2 < 0)
+            {
+                negativeMax = first1 * last1 * last2;
+            }
+
+            return Math.Max(positiveMax, negativeMax);
         }
     }
 }
