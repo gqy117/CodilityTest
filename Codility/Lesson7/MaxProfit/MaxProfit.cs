9c9978f2dd9a4315da35507884f66b0acb61c309
Leo Li <gqy117@gmail.com>
2015-06-15 06:55:42 +0800

    [MaxProfit] Added files.

---
+++ Codility/Lesson7/MaxProfit/MaxProfit.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class MaxProfit
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson7/MaxProfitTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class MaxProfitTest
+    {
+        [Test]
+        [TestCase(new int[] { 23171, 21011, 21123, 21366, 21013, 21367 }, 356)]
+        public void MaxProfit_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MaxProfit.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
