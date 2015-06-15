7ace3fe8da72f5721a0709bdfcc4e3bdd29add10
Leo Li <gqy117@gmail.com>
2015-06-15 15:19:51 +0800

    [MaxDoubleSliceSum] Correctness 50% Performance 57%

--- Codility/Lesson7/MaxDoubleSliceSum/MaxDoubleSliceSum.cs
+++ Codility/Lesson7/MaxDoubleSliceSum/MaxDoubleSliceSum.cs
@@ -6,7 +6,55 @@
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            int X = 0;
+            int Y = 1;
+            int Z = N - 1;
+            int totalLoseCausedByX = A[0];
+            int totalLoseCausedByY = A[1];
+            int totalLoseCausedByZ = A[N - 1];
+            int sum = 0;
+
+            for (int i = 0; i < N - 2; i++)
+            {
+                int current = A[i];
+
+                if (current + sum < totalLoseCausedByX)
+                {
+                    X = i;
+                    Y = X + 1;
+                    totalLoseCausedByY = A[Y];
+                }
+                else if (i > X)
+                {
+                    if (current < totalLoseCausedByY)
+                    {
+                        Y = i; 
+                        totalLoseCausedByY = Math.Min(current, totalLoseCausedByY);
+                    }
+                }
+
+                totalLoseCausedByX = Math.Min(current + sum, totalLoseCausedByX);
+                sum += current;
+            }
+
+            int all = sum + A[N - 1] + A[N - 2];
+
+            sum = 0;
+            for (int i = N - 1; i > X + 1; i--)
+            {
+                int current = A[i];
+
+                if (current + sum < totalLoseCausedByZ)
+                {
+                    Z = i;
+                }
+
+                totalLoseCausedByZ = Math.Min(current + sum, totalLoseCausedByZ);
+                sum += current;
+            }
+
+            return all - totalLoseCausedByX - totalLoseCausedByY - totalLoseCausedByZ;
         }
     }
 }
--- CodilityTest/Lesson7/MaxDoubleSliceSumTest.cs
+++ CodilityTest/Lesson7/MaxDoubleSliceSumTest.cs
@@ -12,6 +12,26 @@
             ExecuteSolution(A, expected);
         }
 
+        [Test]
+        [TestCase(new int[] { 1, -1000, 1, 1, 1, -1000, 1 }, 2)]
+        [TestCase(new int[] { 1, -1000, 1, -1, 1, -1000, 1 }, 2)]
+        [TestCase(new int[] { 1, -1000, 1, -1, -1000, 1, -1000, 1 }, 0)]
+        public void MaxDoubleSliceSum_BigNegativeInTheMiddle(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3 }, 0)]
+        [TestCase(new int[] { 1, 1, 1 }, 0)]
+        [TestCase(new int[] { 3, 2, 1 }, 0)]
+        [TestCase(new int[] { 10000, 10000, 10000 }, 0)]
+        [TestCase(new int[] { -10000, -10000, -10000 }, 0)]
+        public void MaxDoubleSliceSum_N3(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
         private static void ExecuteSolution(int[] A, int expected)
         {
             // Arrange
