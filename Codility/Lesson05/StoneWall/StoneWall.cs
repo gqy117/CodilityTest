fdab20b90d8085cda828260d5d0d9e8e109f413c
Leo Li <gqy117@gmail.com>
2015-06-16 10:47:03 +0800

    [StoneWall] Correctness 100% Performance 77%

--- Codility/Lesson05/StoneWall/StoneWall.cs
+++ Codility/Lesson05/StoneWall/StoneWall.cs
@@ -1,12 +1,41 @@
 ﻿namespace Codility
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class StoneWall
     {
         public static int Solution(int[] H)
         {
-            throw new NotImplementedException();
+            int N = H.Length;
+            IList<int> stack = new List<int>();
+            int block = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                // Current Height
+                int height = H[i];
+
+                if (!stack.Any() || stack.Last() != height)
+                {
+                    // Try to find a low block to stand on
+                    stack = stack.Where(x => x <= height).ToList();
+
+                    // The last block is not satisfied the required height
+                    if (height != stack.LastOrDefault())
+                    {
+                        // Add a block of the same height as required
+                        stack.Add(height);
+
+                        // Use a new block
+                        block++;
+                    }
+                }
+            }
+
+            return block;
         }
     }
 }
