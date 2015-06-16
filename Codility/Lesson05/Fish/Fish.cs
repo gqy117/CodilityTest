5f5d302fcfd2b8b167ecbeff8f0f35fafad6dd17
Leo Li <gqy117@gmail.com>
2015-06-16 13:34:25 +0800

    [Fish] Added files.

---
+++ Codility/Lesson05/Fish/Fish.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class Fish
+    {
+        public static int Solution(int[] A, int[] B)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson05/FishTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class FishTest
+    {
+        [Test]
+        [TestCase(new int[] { 4, 3, 2, 1, 5 }, new int[] { 0, 1, 0, 0, 0 }, 2)]
+        public void Fish_Default(int[] A, int[] B, int expected)
+        {
+            ExecuteSolution(A, B, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int[] B, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Fish.Solution(A, B);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
