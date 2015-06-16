e690c46d687b65580ac79c361a3dfae85a3abf59
Leo Li <gqy117@gmail.com>
2015-06-16 20:47:36 +0800

    [Nesting] Added files.

---
+++ Codility/Lesson05/Nesting/Nesting.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class Nesting
+    {
+        public static int Solution(string S)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson05/NestingTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class NestingTest
+    {
+        [Test]
+        [TestCase("(()(())())", 1)]
+        [TestCase("())", 0)]
+        public void Nesting_Default(string S, int expected)
+        {
+            ExecuteSolution(S, expected);
+        }
+
+        private static void ExecuteSolution(string S, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Nesting.Solution(S);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
