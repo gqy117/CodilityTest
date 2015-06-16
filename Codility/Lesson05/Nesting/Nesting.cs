0f493b542f92950aedf9e2974c5e1e623938f8bb
Leo Li <gqy117@gmail.com>
2015-06-16 21:21:30 +0800

    [Nesting] 100% solution.

--- Codility/Lesson05/Nesting/Nesting.cs
+++ Codility/Lesson05/Nesting/Nesting.cs
@@ -1,12 +1,83 @@
 ﻿namespace Codility
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Nesting
     {
+        private enum Direction
+        {
+            Left = 0,
+            Right = 1
+        }
+
         public static int Solution(string S)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(S))
+            {
+                return 1;
+            }
+
+            IList<char> s = S.ToCharArray().ToList();
+            IList<Bracket> leftBrackets = new List<Bracket>();
+            IList<Bracket> rightBrackets = new List<Bracket>();
+
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                char c = s[i];
+                Bracket bracket = GetBracket(c, i);
+
+                switch (bracket.Direction)
+                {
+                    case Direction.Left:
+                        leftBrackets.Add(bracket);
+                        break;
+                    case Direction.Right:
+                        rightBrackets.Add(bracket);
+                        break;
+                }
+
+                if (leftBrackets.Any() && rightBrackets.Any()
+                    && leftBrackets.Last().Index < rightBrackets.Last().Index)
+                {
+                    leftBrackets.RemoveAt(leftBrackets.Count - 1);
+                    rightBrackets.RemoveAt(rightBrackets.Count - 1);
+                }
+            }
+
+            if (!leftBrackets.Any() && !rightBrackets.Any())
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static Bracket GetBracket(char c, int i)
+        {
+            Bracket bracket = new Bracket() { Index = i };
+
+            switch (c)
+            {
+                case '(':
+                    bracket.Direction = Direction.Left;
+                    break;
+                case ')':
+                    bracket.Direction = Direction.Right;
+                    break;
+                default:
+                    throw new Exception("Invalid char!");
+            }
+
+            return bracket;
+        }
+
+        private class Bracket
+        {
+            public Direction Direction { get; set; }
+
+            public int Index { get; set; }
         }
     }
 }
--- CodilityTest/Lesson05/NestingTest.cs
+++ CodilityTest/Lesson05/NestingTest.cs
@@ -13,6 +13,28 @@
             ExecuteSolution(S, expected);
         }
 
+        [Test]
+        [TestCase("", 1)]
+        public void Nesting_Empty(string S, int expected)
+        {
+            ExecuteSolution(S, expected);
+        }
+
+        [Test]
+        [TestCase(")(", 0)]
+        public void Nesting_InverseDirection(string S, int expected)
+        {
+            ExecuteSolution(S, expected);
+        }
+
+        [Test]
+        [TestCase("))((", 0)]
+        [TestCase("()()", 1)]
+        public void Nesting_InverseDirection2(string S, int expected)
+        {
+            ExecuteSolution(S, expected);
+        }
+
         private static void ExecuteSolution(string S, int expected)
         {
             // Arrange
