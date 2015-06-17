2810e5fbd5a935deac2bebe3af97500e0a040cab
Leo Li <gqy117@gmail.com>
2015-06-17 15:43:05 +0800

    [EquiLeader] Added files.

---
+++ CodilityTest/Lesson06/EquiLeaderTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class EquiLeaderTest
+    {
+        [Test]
+        [TestCase(new int[] { 4, 3, 4, 4, 4, 2 }, 2)]
+        public void EquiLeader_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = EquiLeader.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
