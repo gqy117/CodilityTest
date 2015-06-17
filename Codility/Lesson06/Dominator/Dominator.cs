0c928647725d8a652a3e3a1b87842c62ac33d28d
Leo Li <gqy117@gmail.com>
2015-06-17 15:13:34 +0800

    [Dominator] 100% solution for the first time.

--- Codility/Lesson06/Dominator/Dominator.cs
+++ Codility/Lesson06/Dominator/Dominator.cs
@@ -1,12 +1,47 @@
 ﻿namespace Codility
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Dominator
     {
         public static int Solution(int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            Stack<int> leaderNumber = new Stack<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                // If current one is the same as the last one, then add the current one to the stack
+                if (!leaderNumber.Any() 
+                    || leaderNumber.Peek() == A[i])
+                {
+                    leaderNumber.Push(A[i]);
+                }
+                else
+                {
+                    //// If the last one is different from the current one, then we remove both
+
+                    leaderNumber.Pop();
+                }
+            }
+
+            // There is a possible leader
+            if (leaderNumber.Any())
+            {
+                int possibleLeader = leaderNumber.Pop();
+
+                /*If the number appears more than half of the array, 
+                 * then it's the true leader, 
+                 * and we find its index*/
+                if (A.Count(x => x == possibleLeader) > N / 2)
+                {
+                    return A.ToList().FindIndex(x => x == possibleLeader);
+                }
+            }
+
+            return -1;
         }
     }
 }
--- CodilityTest/Lesson06/DominatorTest.cs
+++ CodilityTest/Lesson06/DominatorTest.cs
@@ -13,6 +13,41 @@
             ExecuteSolution(A, expected);
         }
 
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { -1 })]
+        public void Dominator_NoLeader(int[] A, int[] expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 1, 1, 4, 5, 6 }, new int[] { 0 })]
+        public void Dominator_LeaderAtTheBeginning(int[] A, int[] expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 1, 4, 5, 6 }, new int[] { -1 })]
+        public void Dominator_LeaderCandidateAppearsHalfOfTheArray(int[] A, int[] expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 4, 5, 6, 1, 1, 1, 1 }, new int[] { 3 })]
+        public void Dominator_LeaderAtTheEnd(int[] A, int[] expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 4, 1, 5, 1, 6, 1 }, new int[] { 0 })]
+        public void Dominator_LeaderInTheMiddle(int[] A, int[] expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
         private static void ExecuteSolution(int[] A, int[] expected)
         {
             // Arrange
