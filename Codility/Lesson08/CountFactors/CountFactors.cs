6b64280aed381fc6a0c6e52d850a8efdd5166e42
Leo Li <gqy117@gmail.com>
2015-06-18 15:31:27 +0800

    [CountFactors] Added files.

---
+++ Codility/Lesson08/CountFactors/CountFactors.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class CountFactors
+    {
+        public static int Solution(int N)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson08/CountFactorsTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class CountFactorsTest
+    {
+        [Test]
+        [TestCase(24, 8)]
+        public void CountFactors_Default(int N, int expected)
+        {
+            ExecuteSolution(N, expected);
+        }
+
+        private static void ExecuteSolution(int N, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = CountFactors.Solution(N);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
