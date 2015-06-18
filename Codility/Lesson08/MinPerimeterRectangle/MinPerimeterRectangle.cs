7637b2aed7ac1914754b279aa1889570dcf722ba
Leo Li <gqy117@gmail.com>
2015-06-18 13:44:24 +0800

    [MinPerimeterRectangle] Added files.

---
+++ Codility/Lesson08/MinPerimeterRectangle/MinPerimeterRectangle.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class MinPerimeterRectangle
+    {
+        public static int Solution(int N)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson08/MinPerimeterRectangleTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class MinPerimeterRectangleTest
+    {
+        [Test]
+        [TestCase(30, 22)]
+        public void MinPerimeterRectangle_Default(int N, int expected)
+        {
+            ExecuteSolution(N, expected);
+        }
+
+        private static void ExecuteSolution(int N, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = MinPerimeterRectangle.Solution(N);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
