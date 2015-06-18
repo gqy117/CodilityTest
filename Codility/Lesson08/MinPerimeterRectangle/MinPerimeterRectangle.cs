3199b5b151b7b63b363b6ac837b3cd72b929de94
Leo Li <gqy117@gmail.com>
2015-06-18 15:18:30 +0800

    [MinPerimeterRectangle] The first 100% solution.

--- Codility/Lesson08/MinPerimeterRectangle/MinPerimeterRectangle.cs
+++ Codility/Lesson08/MinPerimeterRectangle/MinPerimeterRectangle.cs
@@ -6,7 +6,22 @@
     {
         public static int Solution(int N)
         {
-            throw new NotImplementedException();
+            int perimeter = int.MaxValue;
+
+            int A = 1;
+
+            while (Math.Pow(A, 2) <= N)
+            {
+                if (N % A == 0)
+                {
+                    int B = N / A;
+                    perimeter = Math.Min((A + B) * 2, perimeter);
+                }
+
+                A++;
+            }
+
+            return perimeter;
         }
     }
 }
--- CodilityTest/Lesson08/MinPerimeterRectangleTest.cs
+++ CodilityTest/Lesson08/MinPerimeterRectangleTest.cs
@@ -12,6 +12,27 @@
             ExecuteSolution(N, expected);
         }
 
+        [Test]
+        [TestCase(1, 4)]
+        public void MinPerimeterRectangle_N1(int N, int expected)
+        {
+            ExecuteSolution(N, expected);
+        }
+
+        [Test]
+        [TestCase(2, 6)]
+        public void MinPerimeterRectangle_N2(int N, int expected)
+        {
+            ExecuteSolution(N, expected);
+        }
+
+        [Test]
+        [TestCase(3, 8)]
+        public void MinPerimeterRectangle_N3(int N, int expected)
+        {
+            ExecuteSolution(N, expected);
+        }
+
         private static void ExecuteSolution(int N, int expected)
         {
             // Arrange
