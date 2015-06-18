add75837921cd1b5c761c2068e93b9b52b5c44f0
Leo Li <gqy117@gmail.com>
2015-06-18 20:52:27 +0800

    [Peaks] Added files.

---
+++ CodilityTest/Lesson08/PeaksTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class PeaksTest
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 }, 3)]
+        public void Peaks_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Peaks.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
