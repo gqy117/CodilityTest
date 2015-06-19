18d5c474ba09186d11d44eca15880a8d325d6d9e
Leo Li <gqy117@gmail.com>
2015-06-19 09:02:48 +0800

    [CountSemiprimes] Added files.

---
+++ Codility/Lesson09/CountSemiprimes/CountSemiprimes.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class CountSemiprimes
+    {
+        public static int[] Solution(int N, int[] P, int[] Q)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson09/CountSemiprimesTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class CountSemiprimesTest
+    {
+        [Test]
+        [TestCase(26, new int[] { 1, 4, 16 }, new int[] { 26, 10, 20 }, new int[] { 10, 0, 4 })]
+        public void CountSemiprimes_Default(int N, int[] P, int[] Q, int expected)
+        {
+            ExecuteSolution(N, P, Q, expected);
+        }
+
+        private static void ExecuteSolution(int N, int[] P, int[] Q, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = CountSemiprimes.Solution(N, P, Q);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
