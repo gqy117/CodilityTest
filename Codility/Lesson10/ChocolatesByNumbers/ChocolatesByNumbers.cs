f4f389090c45bb34020887ed3b3eb7b929d22ea2
Leo Li <gqy117@gmail.com>
2015-06-20 14:53:18 +0800

    [ChocolatesByNumbers] Added files.

---
+++ Codility/Lesson10/ChocolatesByNumbers/ChocolatesByNumbers.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class ChocolatesByNumbers
+    {
+        public static int Solution(int N, int M)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson10/ChocolatesByNumbersTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    public class ChocolatesByNumbersTest
+    {
+        [Test]
+        [TestCase(10, 4, 5)]
+        public void ChocolatesByNumbers_Default(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        private static void ExecuteSolution(int N, int M, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = ChocolatesByNumbers.Solution(N, M);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
