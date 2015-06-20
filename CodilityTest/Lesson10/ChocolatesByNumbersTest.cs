a0e90ac2cccb0b7a4cab59686fe32827c0048522
Leo Li <gqy117@gmail.com>
2015-06-20 15:14:57 +0800

    [ChocolatesByNumbers] Correctness 100% Performance 50%

--- CodilityTest/Lesson10/ChocolatesByNumbersTest.cs
+++ CodilityTest/Lesson10/ChocolatesByNumbersTest.cs
@@ -12,6 +12,55 @@
             ExecuteSolution(N, M, expected);
         }
 
+        [Test]
+        [TestCase(1, 1, 1)]
+        public void ChocolatesByNumbers_N1(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        [Test]
+        [TestCase(1, 4, 1)]
+        public void ChocolatesByNumbers_N1_M4(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        [Test]
+        [TestCase(1, 1000000000, 1)]
+        public void ChocolatesByNumbers_N1_MMax(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        [Test]
+        [TestCase(1000000000, 1000000000, 1)]
+        public void ChocolatesByNumbers_NMax_MMax(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        [Test]
+        [TestCase(1000000000, 1, 1000000000)]
+        public void ChocolatesByNumbers_NMax_M1(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        [Test]
+        [TestCase(1000000000, 1000000000 - 1, 1000000000)]
+        public void ChocolatesByNumbers_NMax_MMaxMinus1(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
+        [Test]
+        [TestCase(10, 9, 10)]
+        public void ChocolatesByNumbers_N10_M9(int N, int M, int expected)
+        {
+            ExecuteSolution(N, M, expected);
+        }
+
         private static void ExecuteSolution(int N, int M, int expected)
         {
             // Arrange
