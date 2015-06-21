e5fa3e51335696d30dd1e1a2ef236e428818f3de
Leo Li <gqy117@gmail.com>
2015-06-21 12:27:56 +0800

    [CountDistinctSlices] Correctness 40% Performance 20%

--- Codility/Lesson13/CountDistinctSlices/CountDistinctSlices.cs
+++ Codility/Lesson13/CountDistinctSlices/CountDistinctSlices.cs
@@ -1,12 +1,63 @@
 ﻿namespace Codility
 {
     using System;
+    using System.Collections.Generic;
 
     public class CountDistinctSlices
     {
         public static int Solution(int M, int[] A)
         {
-            throw new NotImplementedException();
+            int N = A.Length;
+            if (N == 1)
+            {
+                return 1;
+            }
+
+            Dictionary<int, int> distinctValue = new Dictionary<int, int>()
+            {
+                { A[0], A[0] }
+            };
+
+            int start = 0;
+            int end = 0;
+            int count = 0;
+
+            while (end < N - 1 && start < N - 1)
+            {
+                int currentValue = 0;
+
+                while (end < N - 1 && !distinctValue.ContainsKey(A[end + 1]))
+                {
+                    end++;
+                    currentValue = A[end];
+                    distinctValue.Add(currentValue, currentValue);
+                    count++;
+                }
+
+                while (start <= end)
+                {
+                    if (start != end)
+                    {
+                        distinctValue.Remove(A[start]);
+                        start++;
+                        if (start != end)
+                        {
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        distinctValue.Clear();
+                        start++;
+                        end++;
+                        distinctValue.Add(A[start], A[start]);
+                    }
+
+                    break;
+                }
+            }
+
+            return A.Length + count;
         }
     }
 }
--- CodilityTest/Lesson13/CountDistinctSlicesTest.cs
+++ CodilityTest/Lesson13/CountDistinctSlicesTest.cs
@@ -13,6 +13,48 @@
             ExecuteSolution(M, A, expected);
         }
 
+        [Test]
+        [TestCase(6, new int[] { 1 }, 1)]
+        public void CountDistinctSlices_N1(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
+        [Test]
+        [TestCase(6, new int[] { 1, 2 }, 3)]
+        public void CountDistinctSlices_N2(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
+        [Test]
+        [TestCase(6, new int[] { 2, 2 }, 2)]
+        public void CountDistinctSlices_N2_SameValue(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
+        [Test]
+        [TestCase(6, new int[] { 1, 2, 1 }, 5)]
+        public void CountDistinctSlices_N3(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
+        [Test]
+        [TestCase(6, new int[] { 1, 2, 2, 1 }, 6)]
+        public void CountDistinctSlices_N4(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
+        [Test]
+        [TestCase(6, new int[] { 1, 2, 3, 2, 1 }, 11)]
+        public void CountDistinctSlices_N5(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
         private static void ExecuteSolution(int M, int[] A, int expected)
         {
             // Arrange
