84a3b5de1edacad121d1ebef6cdb6d9ad3c8dec4
Leo Li <gqy117@gmail.com>
2015-06-21 09:10:29 +0800

    [AbsDistinct] Added files.

---
+++ Codility/Lesson13/AbsDistinct.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class AbsDistinct
+    {
+        public static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson13/AbsDistinctTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AbsDistinctTest
+    {
+        [Test]
+        [TestCase(new int[] { -5, -3, -1, 0, 3, 6 }, 5)]
+        public void ChocolatesByNumbers_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        private static void ExecuteSolution(int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = AbsDistinct.Solution(A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
