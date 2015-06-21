eaed1696c89b81a72f31ba7c9ed827817ca886a1
Leo Li <gqy117@gmail.com>
2015-06-21 10:21:07 +0800

    [AbsDistinct] 100%.

--- CodilityTest/Lesson13/AbsDistinctTest.cs
+++ CodilityTest/Lesson13/AbsDistinctTest.cs
@@ -8,7 +8,22 @@
     {
         [Test]
         [TestCase(new int[] { -5, -3, -1, 0, 3, 6 }, 5)]
-        public void ChocolatesByNumbers_Default(int[] A, int expected)
+        public void AbsDistinct_Default(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1 }, 1)]
+        [TestCase(new int[] { int.MaxValue }, 1)]
+        public void AbsDistinct_N1(int[] A, int expected)
+        {
+            ExecuteSolution(A, expected);
+        }
+
+        [Test]
+        [TestCase(new int[] { int.MinValue }, 1)]
+        public void AbsDistinct_Min(int[] A, int expected)
         {
             ExecuteSolution(A, expected);
         }
