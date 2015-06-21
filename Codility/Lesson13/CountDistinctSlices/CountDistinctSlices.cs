219d80ac6823ccffcf3e2444d8329d93b5a27f9c
Leo Li <gqy117@gmail.com>
2015-06-21 10:26:07 +0800

    [CountDistinctSlices] Added files.

---
+++ Codility/Lesson13/CountDistinctSlices/CountDistinctSlices.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility
+{
+    using System;
+
+    public class CountDistinctSlices
+    {
+        public static int Solution(int M, int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodilityTest/Lesson13/CountDistinctSlicesTest.cs
@@ -0,0 +1,27 @@
+﻿namespace CodilityTest
+{
+    using Codility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CountDistinctSlicesTest
+    {
+        [Test]
+        [TestCase(6, new int[] { 3, 4, 5, 5, 2 }, 9)]
+        public void CountDistinctSlices_Default(int M, int[] A, int expected)
+        {
+            ExecuteSolution(M, A, expected);
+        }
+
+        private static void ExecuteSolution(int M, int[] A, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = CountDistinctSlices.Solution(M, A);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
